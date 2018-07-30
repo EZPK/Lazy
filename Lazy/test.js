c4bc3e78eb0368b2f74b7e1b096d32981fe177b6
EZPK <thom.bruyer@gmail.com>
2018-07-30 23:57:40 +0200

    adding count modifier to stream method

--- Lazy/test.js
+++ Lazy/test.js
@@ -1,32 +1,38 @@
+// Get job article from data ID
+// https://www.leforem.be/recherche-offres-emploi/jsp/index.jsp#jobDetail?jobId=I30947968
+
+// Link to the REST API
+// https://www.leforem.be/recherche-offres-emploi/rest/searchJob/fromQuickSearch?query=2446349
+
+
+
 let foremnode = require('leforem-node');
 // Public API
 
 let client = new foremnode();
 
-let q = "developpeur",
+let q = "professeur",
  	location="BE-WLG",
- 	page=2,
- 	resultat= 1;
+ 	page=1,
+ 	resultat= 150;
 
-client.search(resultat,page,q,location,function (error, data, count, page, resultat) {
-    if(error) console.log("Error: ",error)
-    let jobs = [];
-    data.map((i)=> {
-        jobs.push(i.title)
-    })
-    // console.log(jobs)
-
-
-    // Get job article from data ID
-    // https://www.leforem.be/recherche-offres-emploi/jsp/index.jsp#jobDetail?jobId=I30947968
-
-    // Link to the REST API
-    // https://www.leforem.be/recherche-offres-emploi/rest/searchJob/fromQuickSearch?query=2446349
-
-
-    console.log(data[0])
-});
-
-// client.stream(q,location,function (data,count,timestamp) {
-// 	console.dir(data);
+// client.search(resultat,page,q,location,function (error, data, count, page, resultat) {
+//     if(error) console.log("Error: ",error)
+//     let jobs = [];
+//     data.map((i)=> {
+//         jobs.push(i.title)
+//     })
+//     console.log(data[0])
 // });
+let counter = 0;
+
+client.stream(q,location, resultat, function (data, count, timestamp) {
+    counter += 1;
+    console.log("Counter: ", counter)
+    console.log('')
+    for(let item in data){
+        console.log(data[item].title.trim());
+    }
+    console.log('###############')
+
+});

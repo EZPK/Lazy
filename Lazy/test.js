c90e6e2e6a9aae0c051e59cf72fd81222e8a1275
Gouze <gouze@me.com>
2018-08-06 17:31:03 +0200

    rectif test.js

--- Lazy/test.js
+++ Lazy/test.js
@@ -16,22 +16,22 @@ let q = "professeur",
  	page=1,
  	resultat= 150;
 
-client.search(resultat,page,q,location,function (error, data, count, page, resultat) {
-    if(error) console.log("Error: ",error)
-    let jobs = [];
-    data.map((i)=> {
-        jobs.push(i.title)
-    })
-    console.log(data[0])
-});
-// let counter = 0;
-
-// client.stream(q,location, resultat, function (data, count, timestamp) {
-//     counter += 1;
-//     console.log("Counter: ", counter)
-//     console.log('')
-//     for(let item in data){
-//         console.log(data[item].title.trim());
-//     }
-//     console.log('###############')
+// client.search(resultat,page,q,location,function (error, data, count, page, resultat) {
+//     if(error) console.log("Error: ",error)
+//     let jobs = [];
+//     data.map((i)=> {
+//         jobs.push(i.title)
+//     })
+//     console.log(data[0])
 // });
+let counter = 0;
+
+client.stream(q,location, resultat, function (data, count, timestamp) {
+    counter += 1;
+    console.log("Counter: ", counter)
+    console.log('')
+    for(let item in data){
+        console.log(data[item].title.trim());
+    }
+    console.log('###############')
+});
